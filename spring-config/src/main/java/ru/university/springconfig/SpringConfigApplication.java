41f1d69575cd153336b9fe64c11da5c1f2d34fd8
Polina Chulkova <shelkova1937@gmail.com>
2022-12-06 19:04:15 +0500

    добавлен gateway

--- spring-config/src/main/java/ru/university/springconfig/SpringConfigApplication.java
+++ spring-config/src/main/java/ru/university/springconfig/SpringConfigApplication.java
@@ -2,8 +2,10 @@ package ru.university.springconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringConfigApplication {
 
     public static void main(String[] args) {
